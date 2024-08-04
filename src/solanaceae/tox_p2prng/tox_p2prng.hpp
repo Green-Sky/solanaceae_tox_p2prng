fd3c542568088480c32aba2a6348fe2367fcfddd
Green Sky <green@g-s.xyz>
2024-08-04 13:02:16 +0200

    add flow and events to interface

--- src/solanaceae/tox_p2prng/tox_p2prng.hpp
+++ src/solanaceae/tox_p2prng/tox_p2prng.hpp
@@ -4,6 +4,8 @@
 
 #include <solanaceae/tox_contacts/tox_contact_model2.hpp>
 
+// implements P2PRNGI for tox
+// both tox friends(1to1) aswell as tox ngc(NtoN) should be supported
 class ToxP2PRNG : public P2PRNGI {
 	ToxContactModel2& _tcm;
 
@@ -14,4 +16,7 @@ class ToxP2PRNG : public P2PRNGI {
 	public: // p2prng
 		std::vector<uint8_t> newGernation(Contact3Handle c, const ByteSpan initial_state_user_data) override;
 		std::vector<uint8_t> newGernationPeers(const std::vector<Contact3Handle>& c_vec, const ByteSpan initial_state_user_data) override;
+
+		P2PRNG::State getSate(const ByteSpan id) override;
+		ByteSpan getResult(const ByteSpan id) override;
 };
